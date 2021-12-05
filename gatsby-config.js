82d07c6bcd5824d65b575bee8fa59e834cefe10a
Subramanian-Ria <riasub08@gmail.com>
2021-12-05 00:07:55 -0500

    remove iframe plugin still seems to work

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,5 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Gatsby Test Site",
   },
-  plugins: [
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [`gatsby-remark-responsive-iframe`],
-      },
-    },
-  ],
+  plugins: [],
 };

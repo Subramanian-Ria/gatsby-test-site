f200bea45269c98b8d939d45ae47ce1bf9d1037c
Subramanian-Ria <riasub08@gmail.com>
2021-12-05 10:45:55 -0500

    testing x-frame bypass web component

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,11 @@ const IndexPage = () => {
       <iframe width="560" height="315" src="https://www.youtube.com/embed/tajDxBaPBBM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <p>test2</p>
       <iframe width="560" height="315" src="https://www.pennovation.upenn.edu/news/how-femmehacks-inspired-200-women-keep-pursuing-tech-careers" title="Test" frameborder="0"></iframe>
-      <p>test6</p>
+      <p>test7</p>
       <embed src="https://www.pennovation.upenn.edu/news/how-femmehacks-inspired-200-women-keep-pursuing-tech-careers" width="200" height="200" />
+      <script src="https://unpkg.com/@ungap/custom-elements-builtin"></script>
+      <script type="module" src="https://unpkg.com/x-frame-bypass"></script>
+      <iframe is="x-frame-bypass" src="https://www.pennovation.upenn.edu/news/how-femmehacks-inspired-200-women-keep-pursuing-tech-careers"></iframe>
     </main>
   )
 }
